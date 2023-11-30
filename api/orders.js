c32033889e3d85aeabb908f0811206cbbd598a38
sagarmounika <sagarmouni5@gmail.com>
2023-12-01 04:46:27 +0530

    api fix

--- api/orders.js
+++ api/orders.js
@@ -3,15 +3,17 @@ const router = express.Router()
 const generateDummyData = require("../utils/generateDummyData.js")
 
 const orders = generateDummyData(100000)
+
 // Sorting orders by lastUpdateTime in ascending order
 const sortedOrders = [...orders].sort(
   (a, b) => new Date(a.lastUpdateTime) - new Date(b.lastUpdateTime)
 )
+
 router.get("/", async (req, res) => {
   try {
     res.json(sortedOrders)
   } catch (error) {
-    console.error(error)
+    console.error("Error in /api/orders:", error)
     return res.status(500).send("Server error")
   }
 })
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const orders = require("./api/orders")
+const ordersRouter = require("./api/orders") // Import the router from the orders module
 const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 3030
@@ -8,35 +8,12 @@ const PORT = process.env.PORT || 3030
 app.use(cors())
 app.use(bodyParser.json())
 
-
-
-
-
-// app.get("/api/order/:orderid", (req, res) => {
-//   const order = orders.find(o => o.id === parseInt(req.params.orderid))
-//   if (!order) {
-//     res.status(404).json({error: "Order not found"})
-//   } else {
-//     res.json(order)
-//   }
-// })
-
-app.get("/api/orders", orders)
 app.get("/", (req, res) => {
   res.send("Welcome to Star Bakery!")
 })
-// app.post("/api/order", (req, res) => {
-//   const order = req.body
-//   order.id = orders.length + 1
-//   orders.push(order)
-//   // Adding the new order to the sortedOrders array
-//   sortedOrders.push(order)
-//   // Sorting the array again after adding a new order
-//   sortedOrders.sort(
-//     (a, b) => new Date(a.lastUpdateTime) - new Date(b.lastUpdateTime)
-//   )
-//   res.json({orderId: order.id})
-// })
+
+// Use the orders router for the "/api/orders" endpoint
+app.use("/api/orders", ordersRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
