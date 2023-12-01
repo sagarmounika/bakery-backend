0855247a0acd8adcbed5faa5c73994cd20529c79
sagarmounika <sagarmouni5@gmail.com>
2023-12-01 10:09:27 +0530

    ver

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const ordersRouter = require("./api/orders") // Import the router from the orders module
-const cors = require("cors")
-const generateDummyData = require("./utils/generateDummyData.js")
-const app = express()
-const PORT = process.env.PORT || 3030
-const orders = generateDummyData(100000)
+const express = require("express");
+const bodyParser = require("body-parser");
+const ordersRouter = require("./api/orders"); // Import the router from the orders module
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3030;
 
-// Sorting orders by lastUpdateTime in ascending order
-const sortedOrders = [...orders].sort(
-  (a, b) => new Date(a.lastUpdateTime) - new Date(b.lastUpdateTime)
-)
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send(sortedOrdersyy)
-})
+  res.send("Welcome to Star Bakery!");
+});
 
 // Use the orders router for the "/api/orders" endpoint
-app.use("/api/orders", ordersRouter)
+app.use("/api/orders", ordersRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
